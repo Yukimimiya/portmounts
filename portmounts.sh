d8e28972c40673e231605e70dd006c4548409cdb
Yukimimiya <taguchi@ff.iij4u.or.jp>
2012-12-23 23:16:25 +0900

    add -f option. to generate fstab entry.

--- portmounts.sh
+++ portmounts.sh
@@ -25,12 +25,14 @@
 #  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 #  SUCH DAMAGE.
 
+FSTAB=0
 PORTSDIR=${PORTSDIR:-/usr/ports}; # CHANGE THIS if needed.
 EXCLUDES="\.git \.svn"
 VERB=0
-while getopts "p:x:v" flag; do
+while getopts "fp:x:v" flag; do
     case $flag in
 	\?) OPT_ERROR=1; break;;
+	f) FSTAB=1; break;;
 	p) PORTSDIR="$OPTARG";;
 	x) EXCLUDES="${EXCLUDES} $OPTARG";;
 	v) VERB=1;;
@@ -40,12 +42,16 @@ done
 shift $(( $OPTIND - 1 ))
 
 if [ $OPT_ERROR ]; then
-    echo >2& "usage: $0 [-p PORTSDIR] [-x EXCLUDES ...] [-v] REPOS ..."
+    echo >2& "usage: $0 [-f] [-p PORTSDIR] [-x EXCLUDES ...] [-v] REPOS ..."
     exit 1
 fi
 
 for i in "$@"; do
-    [ -d "${i}/Mk" ] && mount -t unionfs "${i}/Mk" "${PORTSDIR}/Mk"
+     if [ -d "${i}/Mk" ]; then 
+	[ "x${VERB}" != "x0" ] && echo >2& "Mount: ${i}/Mk  -> ${PORTSDIR}/Mk" 
+	mount -t unionfs "${i}/Mk" "${PORTSDIR}/Mk"
+	[ "x${FSTAB}" != "x0"  ] && printf "${i}/Mk\t${PORTSDIR}/Mk\tunionfs\trw\t0\t0\n"
+    fi
     find $i -name Makefile -type f -depth 3 | while read LINE; do
 	for j in ${EXCLUDES}; do
 	    LINE=`echo "$LINE" | egrep -v -e "${j}"`
@@ -58,10 +64,11 @@ for i in "$@"; do
 	category=${category##*/}
 	if [ -d "${PORTSDIR}/${category}" -a \
 	    ! -d "${PORTSDIR}/${category}/${portname}" ]; then
-		[ ${VERB} ] && echo "New Port: ${category}/${portname}"
+		[ "x${VERB}" != "x0" ] && echo >2& "New Port: ${category}/${portname}"
 		mkdir -p "${PORTSDIR}/${category}/${portname}"
 	fi
-	[ "x${VERB}" != "x0" ] && echo "Mount: ${dirname} -> ${PORTSDIR}/${category}/${portname}"
+	[ "x${VERB}" != "x0" ] && echo 2>& "Mount: ${dirname} -> ${PORTSDIR}/${category}/${portname}"
 	mount -t nullfs "${dirname}" "${PORTSDIR}/${category}/${portname}"
+	[ "x${FSTAB}" != "x0" ] && printf "${dirname}\t${PORTSDIR}/${category}/${portname}\tnullfs\trw\t0\t0\n"
     done
 done
